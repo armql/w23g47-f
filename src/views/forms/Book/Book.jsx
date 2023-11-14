745bcb45760ebaaacd6a14e96a8b05ac1f79f2cf
arm75 <115809877+armql@users.noreply.github.com>
2023-11-14 21:53:55 +0100

    minimal changes

--- src/views/forms/Book/Book.jsx
+++ src/views/forms/Book/Book.jsx
@@ -8,15 +8,19 @@ export default function Book() {
   return (
     <Fragment>
       <div className="flex flex-row items-center justify-center">
-        <div className="flex flex-row z-10 items-center justify-center mx-auto border-2 border-indigo-200 rounded-l-lg shadow-sm">
-          <FromWhere />
-          <WhereTo />
-          <DepartReturn />
-          <PerPerson />
+        <div className="z-10 mx-auto flex flex-col items-center justify-center rounded-l-lg border-2 border-indigo-200 shadow-sm sm:flex-row">
+          <div className="flex h-full w-full flex-col xl:flex-row">
+            <FromWhere />
+            <WhereTo />
+          </div>
+          <div className="flex h-full w-full flex-col xl:flex-row">
+            <DepartReturn />
+            <PerPerson />
+          </div>
         </div>
         <button
           type="button"
-          className="active:scale-100 hover:scale-100 active:translate-x-0 -translate-x-2 active:bg-indigo-400 active:border-indigo-400 hover:bg-indigo-600 font-light hover:text-white hover:border-indigo-600 transition duration-300 border-indigo-200 border-2 w-full h-full py-2 px-4 text-indigo-700 rounded-r-lg scale-95"
+          className="h-full w-full -translate-x-2 scale-95 rounded-r-lg border-2 border-indigo-200 px-4 py-2 font-light text-indigo-700 transition duration-300 hover:scale-100 hover:border-indigo-600 hover:bg-indigo-600 hover:text-white active:translate-x-0 active:scale-100 active:border-indigo-400 active:bg-indigo-400 "
         >
           Search
         </button>
--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -3,9 +3,9 @@ import Book from "../forms/Book/Book";
 
 export default function Home() {
   return (
-    <div className="relative w-screen h-screen bg-indigo-50">
+    <div className="relative h-screen w-screen bg-indigo-50">
       <div>Header Goes here</div>
-      <div className="flex flex-col items-center justify-center h-full">
+      <div className="flex h-full flex-col items-center justify-center">
         <Book />
       </div>
     </div>
