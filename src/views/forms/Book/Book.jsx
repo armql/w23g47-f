79d659b191d3b3e2e85ccca86435c5c531de3293
arm75 <115809877+armql@users.noreply.github.com>
2023-11-14 11:27:32 +0100

    layers

--- src/views/forms/Book/Book.jsx
+++ src/views/forms/Book/Book.jsx
@@ -7,8 +7,8 @@ import DepartReturn from "./BookInputs/DepartReturn";
 export default function Book() {
   return (
     <Fragment>
-      <div className="flex flex-row items-center justify-center mt-20">
-        <div className="flex flex-row items-center justify-center mx-auto border-2 border-indigo-200 rounded-l-lg shadow-sm">
+      <div className="flex flex-row items-center justify-center">
+        <div className="flex flex-row z-10 items-center justify-center mx-auto border-2 border-indigo-200 rounded-l-lg shadow-sm">
           <FromWhere />
           <WhereTo />
           <DepartReturn />
@@ -16,7 +16,7 @@ export default function Book() {
         </div>
         <button
           type="button"
-          className="active:bg-indigo-400 active:border-indigo-400 hover:bg-indigo-600 font-light hover:text-white hover:border-indigo-600 transition duration-100 border-indigo-200 border-2 w-full h-full py-2 px-4 text-indigo-700 rounded-r-lg shadow-sm"
+          className="active:scale-100 hover:scale-100 active:translate-x-0 -translate-x-2 active:bg-indigo-400 active:border-indigo-400 hover:bg-indigo-600 font-light hover:text-white hover:border-indigo-600 transition duration-300 border-indigo-200 border-2 w-full h-full py-2 px-4 text-indigo-700 rounded-r-lg scale-95"
         >
           Search
         </button>
