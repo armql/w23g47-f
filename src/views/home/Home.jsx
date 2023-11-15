ecf69ac8e7ae3fc16028b7a4c198b77c0d3abee8
arm75 <115809877+armql@users.noreply.github.com>
2023-11-15 16:09:44 +0100

    home update

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -3,10 +3,14 @@ import Book from "../forms/Book/Book";
 
 export default function Home() {
   return (
-    <div className="relative h-screen w-screen bg-transparent">
-      <div>Header Goes here</div>
-      <div className="flex h-full flex-col items-center justify-center">
-        <Book />
+    <div className="relative h-full w-full bg-transparent">
+      <div className="flex h-screen w-full flex-col items-center justify-center gap-12 ">
+        <div className="text-center text-4xl font-bold text-indigo-700 dark:text-white">
+          Header Goes here
+        </div>
+        <div>
+          <Book />
+        </div>
       </div>
     </div>
   );
