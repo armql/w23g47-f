dc4bbf2b559080ead92cf6a533ee1c42946b7fb1
arm75 <115809877+armql@users.noreply.github.com>
2023-11-15 21:53:47 +0100

    landing page

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <div className="relative h-full w-full bg-transparent">
       <div className="flex h-screen w-full flex-col items-center justify-center gap-12 ">
-        <div className="text-center text-4xl font-bold text-indigo-700 dark:text-white">
-          Header Goes here
+        <div className="w-[500px] bg-gradient-to-r from-purple-600 to-indigo-600 bg-clip-text px-2 text-center text-4xl font-bold text-transparent dark:from-violet-200 dark:to-violet-400 sm:px-0 md:h-36 md:w-[600px] md:text-5xl lg:w-[800px] xl:h-52 xl:text-6xl">
+          Because getting there should be as fun as being there.
         </div>
         <div>
           <Book />
